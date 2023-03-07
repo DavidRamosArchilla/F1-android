c5ab850728096a7231c0f9d9c777dddecf68068e
Davidram1802 <56789396+Davidram1802@users.noreply.github.com>
2023-03-07 23:12:22 +0100

    añadidos 2 endpoins, carreras de un año y resultados de una carrera

--- app/src/main/java/com/example/f1/IF1ApiService.java
+++ app/src/main/java/com/example/f1/IF1ApiService.java
@@ -4,10 +4,17 @@ import com.google.gson.JsonObject;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 @SuppressWarnings("Unused")
 public interface IF1ApiService {
 
     @GET("/api/f1/current/last/results.json")
     Call<JsonObject> getLastRace(); //(@Path("id") long id)
+
+    @GET("/api/f1/{year}")
+    Call<JsonObject> getRacesOfYear(@Path("year") String year);
+
+    @GET("/api/f1/{year}/{round}/results")
+    Call<JsonObject> getResultOfRace(@Path("year") String year, @Path("round") String round);
 }
--- app/src/main/java/com/example/f1/MainActivity.java
+++ app/src/main/java/com/example/f1/MainActivity.java
@@ -2,119 +2,23 @@ package com.example.f1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.StrictMode;
-import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-
-
-import java.util.ArrayList;
-import java.util.List;
-
-import okhttp3.OkHttpClient;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-    private ArrayAdapter<Rowitem> adaptador;
-    private ListView listView;
-    private static final String API_BASE_URL = "https://ergast.com";
-    private IF1ApiService service;
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-//
-//        StrictMode.setThreadPolicy(policy);
-        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(API_BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(httpClient.build())
-                .build();
-
-        service = retrofit.create(IF1ApiService.class);
-//        crearListView();
-        llamarApi();
-    }
-    public void crearListView(){
-        llamarApi();
-    }
-    private List<Rowitem> getRowList(){
-        List<Rowitem> lista_filas = new ArrayList<Rowitem>();
-        for(int i = 0;i<5;i++){
-            lista_filas.add(new Rowitem("Piloto "+ i, i));
-        }
-        return lista_filas;
-    }
-    private void llamarApi(){
-        Call<JsonObject> callAsync = service.getLastRace();
-        Context c = this;
-        callAsync.enqueue(new Callback<JsonObject>() {
+        Button b = findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
-                JsonObject respuesta = response.body();
-                List<Rowitem> filas = crearRowItems(respuesta);
-                adaptador = new RowArrayAdapter(c,
-                        R.layout.row_layout, filas);
-                listView = (ListView) findViewById(R.id.listview);
-                listView.setAdapter(adaptador);
-            }
-
-            @Override
-            public void onFailure(Call<JsonObject> call, Throwable t) {
-                Log.e("error", "error", t);
-                Toast.makeText(
-                        getApplicationContext(),
-                        "ERROR: " + t.getMessage(),
-                        Toast.LENGTH_LONG
-                ).show();
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainActivity.this, ActivityUltimaCarrera.class);
+                startActivity(myIntent);
             }
         });
     }
-
-    private List<Rowitem> crearRowItems(JsonObject respuesta) {
-        List<Rowitem> listaFilas = new ArrayList<Rowitem>();
-        JsonObject carrera = respuesta.getAsJsonObject("MRData")
-                .getAsJsonObject("RaceTable")
-                .getAsJsonArray("Races")
-                .get(0)
-                .getAsJsonObject();
-        JsonArray resultados = carrera.getAsJsonArray("Results");
-        for(int i = 0; i < resultados.size(); i++){
-            JsonObject piloto = resultados.get(i).getAsJsonObject();
-            JsonObject datosPiloto = piloto.get("Driver").getAsJsonObject();
-            listaFilas.add(new Rowitem(datosPiloto.get("familyName").getAsString(),
-                    piloto.get("points").getAsInt()));
-        }
-        return listaFilas;
-    }
-
-    private void callback(){
-        List<Rowitem> filas = getRowList();
-        adaptador = new RowArrayAdapter(this,
-                R.layout.row_layout, filas);
-        listView = (ListView) findViewById(R.id.listview);
-        listView.setAdapter(adaptador);
-    }
 }
-//        try {
-////            Response<String> response = callSync.execute();
-////            String apiResponse = response.body();
-////            Log.e("llamada api: \n", apiResponse);
-//              Log.i("llamada", callAsync.execute().body().toString());
-//        } catch (Exception ex) {
-//            Log.e("error", "error", ex);
-//            ex.printStackTrace();
-//        }
