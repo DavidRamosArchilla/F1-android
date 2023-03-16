3a5c80321ddb6b02de32f38eb9bf3d8e9bfda871
Oscar <oscar.roma.calvo@alumnos.upm.es>
2023-03-16 17:11:05 +0100

    Añadido login por gmail y anonimo. Añadido boton de logout.

--- app/src/main/java/com/example/f1/MainActivity.java
+++ app/src/main/java/com/example/f1/MainActivity.java
@@ -1,78 +1,107 @@
 package com.example.f1;
 
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
-import com.firebase.ui.auth.IdpResponse;
-import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
-import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
-    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
-            new FirebaseAuthUIActivityResultContract(),
-            result -> onSignInResult(result)
-    );
-    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
-        IdpResponse response = result.getIdpResponse();
-        if (result.getResultCode() == RESULT_OK) {
-            // Successfully signed in
-            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-            Toast.makeText(this, "Logueado correctamente", Toast.LENGTH_SHORT).show();
-            // ...
-        } else {
-            // Sign in failed. If response is null the user canceled the
-            // sign-in flow using the back button. Otherwise check
-            // response.getError().getErrorCode() and handle the error.
-            // ...
-            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
-        }
-    }
+    final static String LOG_TAG = "btb";
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private static final int RC_SIGN_IN = 2022;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button b = findViewById(R.id.button);
+        Button btnUltimaCarrera = findViewById(R.id.btn_UltimaCarrera);
 
-        List<AuthUI.IdpConfig> providers = Arrays.asList(
-                new AuthUI.IdpConfig.EmailBuilder().build(),
-                new AuthUI.IdpConfig.GoogleBuilder().build()
-                );
+        Button btnLogOut = findViewById(R.id.btn_LogOut);
 
-        Intent signInIntent = AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(providers)
-                .build();
-        signInLauncher.launch(signInIntent);
-        b.setOnClickListener(new View.OnClickListener() {
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
-            public void onClick(View view) {
-                Intent myIntent = new Intent(MainActivity.this, ActivityUltimaCarrera.class);
-                startActivity(myIntent);
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // user is signed in
+                    CharSequence username = user.getDisplayName();
+                    Toast.makeText(MainActivity.this, getString(R.string.firebase_user_fmt, username), Toast.LENGTH_LONG).show();
+
+                } else {
+                    // user is signed out
+                    startActivityForResult(
+                            // Get an instance of AuthUI based on the default app
+                            AuthUI.getInstance().
+                                    createSignInIntentBuilder().
+                                    setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.GoogleBuilder().build(),
+                                            new AuthUI.IdpConfig.AnonymousBuilder().build()
+
+                                            //new AuthUI.IdpConfig.EmailBuilder().build()
+                                    )).
+                                    setIsSmartLockEnabled(!BuildConfig.DEBUG /* credentials */, true /* hints */).
+                                    //setIsSmartLockEnabled(!BuildConfig.DEBUG /* credentials */, true /* hints */).
+                                            build(),
+                            RC_SIGN_IN
+                    );
+
+                    //Remains logged in when unique gmail account
+                    // setIsSmartLockEnabled(!BuildConfig.DEBUG /* credentials */, true /* hints */).
+                    //setIsSmartLockEnabled(BuildConfig.DEBUG /* credentials */, true /* hints */).
+
+                }
             }
-        });
+        };
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, R.string.signed_in, Toast.LENGTH_SHORT).show();
+                Log.i(LOG_TAG, "onActivityResult " + getString(R.string.signed_in));
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, R.string.signed_cancelled, Toast.LENGTH_SHORT).show();
+                Log.i(LOG_TAG, "onActivityResult " + getString(R.string.signed_cancelled));
+                finish();
+            }
+        }
+    }
+    public void abrirUltimaCarrera (View view){
+        Intent myIntent = new Intent(MainActivity.this, ActivityUltimaCarrera.class);
+        startActivity(myIntent);
+    }
+
+    public void logOut(View v) {
+        mFirebaseAuth.signOut();
+        Log.i(LOG_TAG, getString(R.string.signed_out));
     }
 
 
-    // Para logout
-    /*AuthUI.getInstance()
-        .signOut(this)
-        .addOnCompleteListener(new OnCompleteListener<Void>() {
-            public void onComplete(@NonNull Task<Void> task) {
-                // ...
-            }
-        });*/
 }
