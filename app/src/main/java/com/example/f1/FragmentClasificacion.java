fa4607525923bd16a3f4d98183629fa226005296
jesus.calzas.bedoya <91942107+jesuscalzasbedoya@users.noreply.github.com>
2023-04-19 12:45:29 +0200

    Primeros cambios clasificacion

--- app/src/main/java/com/example/f1/FragmentClasificacion.java
+++ app/src/main/java/com/example/f1/FragmentClasificacion.java
@@ -4,9 +4,23 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -46,6 +60,12 @@ public class FragmentClasificacion extends Fragment {
         return fragment;
     }
 
+    private View view;
+
+    private ArrayAdapter<Rowitem_clasificacionCarrera> adaptador;
+
+    private ListView listView;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,6 +73,8 @@ public class FragmentClasificacion extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        Callback<JsonObject> callback = crearCallback();
+        ((PantallaInicioActivity)getActivity()).getService().getStanding("current").enqueue(callback);
     }
 
     @Override
@@ -61,4 +83,63 @@ public class FragmentClasificacion extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_clasificacion, container, false);
     }
+
+
+
+    private Callback<JsonObject> crearCallback(){
+        return new Callback<JsonObject>() {
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                JsonObject respuesta = response.body();
+                List<Rowitem_clasificacionCarrera> filas = crearRowItems(respuesta);
+                if (getActivity()!=null)
+                    adaptador = new RowArrayAdapter_clasificacionCarrera(getActivity(),
+                            R.layout.row_lista_carreras, filas);
+
+                System.out.println(listView);
+                listView = (ListView) view.findViewById(R.id.listviewListaCarreras);
+                System.out.println(listView);
+                listView.setAdapter(adaptador);
+
+            }
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+                Log.e("error", "error", t);
+                Toast.makeText(
+                        getActivity(),
+                        "ERROR: " + t.getMessage(),
+                        Toast.LENGTH_LONG
+                ).show();
+            }
+        };
+    }
+
+    private List<Rowitem_clasificacionCarrera> crearRowItems(JsonObject respuesta) {
+        List<Rowitem_clasificacionCarrera> listaFilas = new ArrayList<Rowitem_clasificacionCarrera>();
+        JsonArray clasificacion = respuesta.getAsJsonObject("MRData")
+                .getAsJsonObject("StandingsTable")
+                .getAsJsonArray("StandingsLists");
+        for(int i = 0; i < clasificacion.size(); i++){
+            JsonObject datosP = clasificacion.get(i).getAsJsonObject();
+            listaFilas.add(new Rowitem_clasificacionCarrera(
+                    datosP.get("DriverStandings.Driver.driverId").getAsString(),
+                    datosP.get("DriverStandings.points").getAsInt()));
+        }
+        return listaFilas;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
